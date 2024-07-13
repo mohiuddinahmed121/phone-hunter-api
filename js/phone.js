41980003b4459906b3be67cff72beb0f982320f8
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2024-07-13 20:06:56 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,41 @@
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    const phones = data.data;
+    displayPhones(phones);
+}
+
+const displayPhones = phone => {
+
+    const phoneContainer = document.getElementById('phone-container');
+
+    phone.forEach(phone => {
+        console.log(phone);
+        // 1 create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        // 3 set inner html
+        phoneCard.innerHTML = `
+        <figure>
+        <img src="${phone.image}" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        // 4 append child
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
+
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
